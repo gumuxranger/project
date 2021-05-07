ad1985cfc85e78a5aeb2827de14b1407d107fb25
gumuxranger <83735917+gumuxranger@users.noreply.github.com>
2021-05-08 05:09:56 +0700

    Update safelinkutama.js

--- safelinkutama.js
+++ safelinkutama.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="https://safelink.kpu-pemalangkab.go.id/2018/09/practice-environmental-qwareness-to-our.html"}
-if(x=="2"){xxx="https://safelink.kpu-pemalangkab.go.id/2018/09/advantages-of-being-home-business-owner.html"}
-if(x=="3"){xxx="https://safelink.kpu-pemalangkab.go.id/2018/09/10-tips-for-start-small-business-startup.html"}
-if(x=="4"){xxx="https://safelink.kpu-pemalangkab.go.id/2018/09/the-way-to-start-business-at-home.html"}a_to_ve[i].href="https://safelink.kpu-pemalangkab.go.id/2018/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="https://safelink.kpu-pemalangkab.go.id/2021/05/penelitian-pasca-pilkada-2020.html"}
+if(x=="2"){xxx="https://safelink.kpu-pemalangkab.go.id/2021/05/rakor-pemutakhiran-dpt-berkelanjutan.html"}
+if(x=="3"){xxx="https://safelink.kpu-pemalangkab.go.id/2021/05/kunjungan-kerja-kpu-pemalang.html"}
+if(x=="4"){xxx="https://safelink.kpu-pemalangkab.go.id/2021/05/rapat-koordinasi-bako-humas.html"}a_to_ve[i].href="https://safelink.kpu-pemalangkab.go.id/2021/05/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
